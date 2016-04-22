5e43f536afb65dc196be472b72f2a54e81422de2
RangerMauve <RangerMauve@htomail.com>
2016-04-21 23:26:03 -0400

    Initial version with eye

---
+++ index.js
@@ -0,0 +1,11 @@
+"use strict";
+var aframe = require('aframe');
+
+var GamepadControls = require('aframe-gamepad-controls');
+aframe.registerComponent('gamepad-controls', GamepadControls);
+
+require('aframe-text-component');
+
+require('aframe-template-component');
+
+require('aframe-layout-component');
